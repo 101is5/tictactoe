b7ce845a95c45f28b90669ac1c43e89b74f22bd9
101is5 <helleeno@gmail.com>
2022-11-22 16:38:23 -0300

    asjajaaj

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title(props) {
     return (
         <div className="flex flex-col items-center justify-center rounded text-blue-300 font-['Black bison'] bg-cyan-900 italic font-extrabold text-5xl text-center py-8 my-10 w-40">
-            <div>Tic Tac Toe!</div>
+            <div>Cu!</div>
             <div className="font-['Helvetica'] text-xs mt-4 -mb-3 not-italic font-semibold">
                 Jogo da Velha(BR)
             </div>
