c8ade523205754da97d8f806ee131ce6d05ad84b
101is5 <helleeno@gmail.com>
2022-11-25 09:32:33 -0300

    clean App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { useEffect } from "react"
 
 export default function App(props) {
 
-    let load = true;
-
-    // if(window.assign()) load = true;
-
     return (
         <div id="main" className="flex flex-col items-center">
             <Title></Title>
